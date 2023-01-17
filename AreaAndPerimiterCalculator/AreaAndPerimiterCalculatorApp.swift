f0337bef475a3ff4c1b44c0b2c8dba4f448ae887
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-17 11:38:46 -0500

    Set up an empty view to calculate area of a rectangle

--- AreaAndPerimiterCalculator/AreaAndPerimiterCalculatorApp.swift
+++ AreaAndPerimiterCalculator/AreaAndPerimiterCalculatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AreaAndPerimiterCalculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RectangleView()
         }
     }
 }
