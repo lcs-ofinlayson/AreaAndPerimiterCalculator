acd1a1b7a66cee2212d048eb60b978abcbe03bd2
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-17 11:34:40 -0500

    Initial Commit

---
+++ AreaAndPerimiterCalculator/AreaAndPerimiterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimiterCalculatorApp.swift
+//  AreaAndPerimiterCalculator
+//
+//  Created by Oliver Finlayson on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimiterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
